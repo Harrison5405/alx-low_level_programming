7d9aa1b41d1d0391ba5b602506ec4ce28c05e1a9
Harrison5405 <harrisonwaithaka5405@gmail.com>
2023-10-06 14:20:56 -0700

    nf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+#include <stdio.h>
+int main(void)
+{int integerType;
+char charType;
+float floatType;
+double doubleType;
+printf("Size of int is: %ld", sizeof(integerType));
+printf("\nSize of char is: %ld", sizeof(charType));
+printf("\nSize of float is: %ld", sizeof(floatType));
+printf("\nSize of double is: %ld", sizeof(doubleType));
+return 0;
+}
