d4c95291875e09b968f0400490017f6cd96182cb
Harrison5405 <harrisonwaithaka5405@gmail.com>
2023-10-06 15:17:34 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,15 @@
 #include <stdio.h>
 int main(void)
 {
-int integerType;
-char charType;
-float floatType;
+char a;
+int b;
 long int c;
 long long int d;
-print("size of char: %lu bytes \n", (unsigned long)sizeof(charrType));
-print("size of int: %lu bytes \n", (unsigned long)sizeof(intergerType));
+float f;
+print("size of char: %lu bytes \n", (unsigned long)sizeof(a));
+print("size of int: %lu bytes \n", (unsigned long)sizeof(b));
 print("size of long int: %lu bytes \n", (unsigned long)sizeof(c));
 print("size of long long int: %lu bytes \n", (unsigned long)sizeof(d));
-print("size of float: %lu bytes \n", (unsigned long)sizeof(floatType));
+print("size of float: %lu bytes \n", (unsigned long)sizeof(f));
 return (0);
 }
